654a7e22a3b8f006c0416d5f860a257cb42234d2
sethm.sde@gmail.com <sethm.sde@gmail.com>
2021-05-11 20:41:19 +0000

    added a while loop to continue until the goblin is defeated

--- assignments/goblinslayer.py
+++ assignments/goblinslayer.py
@@ -2,15 +2,29 @@
 
 print('A goblin approaches! Roll to attack!')
 
-print('What did you roll?')
-roll = input(">")
-int_roll = int(roll)
+goblin_AC=15
+goblin_HP=7
 
-if int_roll == 20:
-	print('Critical hit! The goblin is vanquished!')
-elif 15 <= int_roll <= 19 or int_roll > 20:
-	print('You hit the goblin, it looks angry...')
-elif 2<= int_roll < 15:
-	print('You missed the goblin. You call yourself an adventurer...')
-elif int_roll == 1:
-    print('Critical miss. You took your own arrow to the knee...')
+while goblin_HP > 0:
+    dmg_prompt = 'How much damaged did you do?'
+    roll_prompt = 'What did you roll?'
+    print(roll_prompt)
+    roll = input('>')
+    int_roll = int(roll)
+
+    if int_roll == 20:
+        print('Critical hit! The goblin is badly injured!')
+        print(dmg_prompt)
+        damage = input('>')
+        goblin_HP -= int(damage)
+    elif goblin_AC <= int_roll <= 19 or int_roll > 20:
+        print('You hit the goblin, it looks angry...')
+        print(dmg_prompt)
+        damage = input('>')
+        goblin_HP -= int(damage)
+    elif 2<= int_roll < 15:
+        print('You missed the goblin. You call yourself an adventurer...')
+    elif int_roll == 1:
+        print('Critical miss. You took your own arrow to the knee...')
+    if goblin_HP <= 0:
+        print('The goblin is vanquished! HUZZAH!')
